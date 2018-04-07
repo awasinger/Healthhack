0f6b507748b1173ef1268a65e7438cd8a2ac20d8
Alex Wasinger <25324676+awasinger@users.noreply.github.com>
2018-04-07 13:14:22 -0500

    Start on form submit

--- resources/views/checkup.blade.php
+++ resources/views/checkup.blade.php
@@ -1,40 +1,47 @@
 @extends('layouts.app')
 @section('content')
-  <h2>Hello, NAME HERE</h2>
-<h3>Your doctor NAME HERE sent you this daily form about your Hypertension to checkup on you.
+  <h2>Hello, {{ $user->name }}</h2>
+<h3>Your doctor {{-- $user->doctor->name --}} sent you this daily form about your Hypertension to checkup on you.
   Please answer these questions so your doctor can know how you are feeling</h3>
-<form class="dailyForm" action="">
+<form class="dailyForm" action="/form/{{$token}}" method="post">
+    {{csrf_field()}}
   <div class="medicationsTaken textBox">
-    <h3 class="question">What medications have taken so far today:</h3>
+    <h3 class="question">{{ $questions[0] }}</h3>
     <input type="text" name="medicationsTaken">
   </div>
+  
   <div class="medicationsToTake textBox">
-    <h3 class="question">What medications are you going to take today:</h3>
+    <h3 class="question">{{ $questions[1] }}</h3>
     <input type="text" name="medicationsToTake">
   </div>
+  
+  
   <div class="currentStress dropDown">
-    <h3 class="question">Are you under any stress today:</h3>
-    <select>
-      <option value="noStress">Not at all</option>
-      <option value="lowStress">A little stress</option>
-      <option value="mediumStress">Medium amount of stress</option>
-      <option value="highStress">A lot of stress</option>
-      <option value="superHighStress">A ton of stress</option>
+    <h3 class="question">{{ $questions[2] }}</h3>
+    <select name="stress">
+        <option value="noStress">Not at all</option>
+        <option value="lowStress">A little stress</option>
+        <option value="mediumStress">Medium amount of stress</option>
+        <option value="highStress">A lot of stress</option>
+        <option value="superHighStress">A ton of stress</option>
     </select>
   </div>
+  
+  
   <div class="heartPalpatations dropDown">
-    <h3 class="question">Have you felt any heart palpatations today:</h3>
-    <select>
+    <h3 class="question">{{ $questions[3] }}</h3>
+    <select name="heart">
       <option value="noPalpatations">No</option>
       <option value="lightPalpatations">A very light palpation</option>
-      <option value="mediumPalpatations">Medium palpatations</option>
-      <option value="severePalpatations">Severe palpatations</option>
-      <option value="verySeverePalpatations">Very severe palapatations(medical help needed)</option>
+      <option value="mediumPalpatations">Medium palpitations</option>
+      <option value="severePalpatations">Severe palpitations</option>
+      <option value="verySeverePalpatations">Very severe palapatations (medical help needed)</option>
     </select>
   </div>
+  
+  
   <div class="heartRate textBox">
-    <h3 class="question">If you have a smart watch or other device, what is your current
-      heart rate:</h3>
+    <h3 class="question">{{ $questions[4] }}</h3>
     <input type="text" name="heartRate">
   </div>
   <input type="submit" value="Submit">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,8 @@
 @extends('layouts.app')
 @section('content')
-<<<<<<< HEAD
-test
-<a href="/meds">Test
-</a>
-=======
-<a href="checkup.blade.php">hel</a>
->>>>>>> 5356b102f1263805b02b37d1272114a5831fc492
+<section>
+    <div class="container">
+    test
+    </div>
+</section>
 @endsection
