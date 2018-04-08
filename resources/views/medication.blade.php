c3b70a1133a212052ed62ca1a90489081add640d
37bla <jegioia37@gmail.com>
2018-04-07 19:55:37 -0500

    correction

--- resources/views/medication.blade.php
+++ resources/views/medication.blade.php
@@ -7,15 +7,15 @@
   <form>
     <div class="form-group">
     <label>What prescribed medications do you take?*</label>
-    <input type="email" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
+    <input type="text" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">When do you take these medications?*</label>
-      <input type="password" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
+      <input type="text" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Any notes for your doctor/caregiver?</label>
-      <input type="password" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
+      <input type="text" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>
