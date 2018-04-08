25c334b0a3f6d7dff7debe5d8463a3cae737bf27
37bla <jegioia37@gmail.com>
2018-04-07 20:04:00 -0500

    added diabetes

--- resources/views/diabetes.blade.php
+++ resources/views/diabetes.blade.php
@@ -1,1 +1,21 @@
 <h2>Diabetes</h2>
+<div class="form-group">
+  <label>What medications have you taken today:</label>
+  <input type="text" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
+</div>
+<div class="form-group">
+  <label>What medications will you take later today:</label>
+  <input type="text" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
+</div>
+<div class="form-group">
+  <label>Did you/will you have an unusual amount:</label>
+  <input type="text" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
+</div>
+<div class="form-group">
+  <label>Do you have your insulin on you:</label>
+  <input type="text" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
+</div>
+<div class="form-group">
+  <label>What is your glucose leve:</label>
+  <input type="text" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
+</div>
