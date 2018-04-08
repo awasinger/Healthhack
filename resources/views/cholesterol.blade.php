7359dde39206f798f02ea86aaaaee54fc2b67bac
37bla <jegioia37@gmail.com>
2018-04-07 20:30:42 -0500

    highCholesterol

---
+++ resources/views/cholesterol.blade.php
@@ -0,0 +1,21 @@
+<h2>High cholesterol</h2>
+<div class="form-group">
+  <label>What medications have you taken today:</label>
+  <input type="text" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
+</div>
+<div class="form-group">
+  <label>What medications will you take later today:</label>
+  <input type="text" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
+</div>
+<div class="form-group">
+  <label>Have you/will you have a unusual amount of fat today:</label>
+  <input type="text" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
+</div>
+<div class="form-group">
+  <label>Have you had any unusual fatigue or shortness of breath today:</label>
+  <input type="text" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
+</div>
+<div class="form-group">
+  <label>Have you done any light aerobic activities today:</label>
+  <input type="text" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
+</div>
