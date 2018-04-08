a3f982f21b992bfa9499acc230bb1811ca59c685
Jessica Chernyak <jessica@Jessicas-MacBook-Pro.local>
2018-04-07 20:28:42 -0500

    hypertension

---
+++ resources/views/hypertension.blade.php
@@ -0,0 +1,21 @@
+<h2>Hypertension</h2>
+<div class="form-group">
+  <label>What medications have you taken today:</label>
+  <input type="text" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
+</div>
+<div class="form-group">
+  <label>What medications will you take later today:</label>
+  <input type="text" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
+</div>
+<div class="form-group">
+  <label>How much stress are you under now:</label>
+  <input type="text" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
+</div>
+<div class="form-group">
+  <label>Have you had any heart palpatations today? If so, explain:</label>
+  <input type="text" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
+</div>
+<div class="form-group">
+  <label>If you have a smart device, what is your current heart rate:</label>
+  <input type="text" class="form-control col-md-4 col-form-label text-md-left" placeholder="">
+</div>
